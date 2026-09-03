107b6be58ff3e6c712b2b3706d31d2a260de790c
unknown <profserv911@gmail.com>
2026-09-03 21:41:54 +0300

    Update working firmware with foam anti-retrigger protection

--- main.c
+++ main.c
@@ -28,22 +28,17 @@
 // ============================================================
 // BOARD CONNECTIONS
 //
-// PIC16F1824 SOIC-14 shifted by one pad on original SOP-16 PCB:
+// PIC16F1824 SOIC-14 shifted by one pad on old SOP-16:
 //
 // PIC 1  VDD -> old pad 2 -> jumper to old pad 1 (+ battery)
 // PIC 5  RC5 -> old pad 6  MOTOR
 // PIC 6  RC4 -> old pad 7  MODE SENSOR
 // PIC 7  RC3 -> old pad 8  IR LED
-// PIC 8  RC2 -> old pad 9  RED LED
-// PIC 10 RC0 -> old pad 11 GREEN LED
-// PIC 11 RA2 -> old pad 12 BLUE LED
-// PIC 13 RA0 -> old pad 14 PHOTO SENSOR
-// PIC 14 VSS -> old pad 15 -> jumper to old pad 16 (GND)
-//
-// NOTE:
-// If the cover sensor is converted to real capacitive sensing,
-// route the sensor electrode to RA4/CPS3 instead of RC4.
-// This source currently uses the existing digital MODE_SENSOR input.
+// PIC 8  RC2 -> old pad 9  RED
+// PIC 10 RC0 -> old pad 11 GREEN
+// PIC 11 RA2 -> old pad 12 BLUE
+// PIC 13 RA0 -> old pad 14 PHOTO
+// PIC 14 VSS -> old pad 15 -> jumper to old pad 16 GND
 //
 // ============================================================
 
@@ -60,49 +55,72 @@
 // SETTINGS
 // ============================================================
 
-// Motor: 0 = ON, 1 = OFF
-#define MOTOR_ON_LEVEL        0
-#define MOTOR_OFF_LEVEL       1
+// Motor is active LOW
+#define MOTOR_ON_LEVEL       0
+#define MOTOR_OFF_LEVEL      1
 
-// RGB LEDs: 0 = ON, 1 = OFF
-#define LED_ON_LEVEL          0
-#define LED_OFF_LEVEL         1
+// RGB channels are active LOW
+#define LED_ON_LEVEL         0
+#define LED_OFF_LEVEL        1
 
-// Cover/mode sensor: 0 = finger present, 1 = no finger
-#define MODE_ACTIVE_LEVEL     0
+// Mode sensor:
+// VCC = no finger
+// 0V  = finger present
+#define MODE_ACTIVE_LEVEL    0
 
-// IR transmitter polarity
-#define IR_ACTIVE_HIGH        1
+/*
+ * IR transmitter polarity.
+ * Keep exactly as in the previous working version.
+ */
+#define IR_ACTIVE_HIGH       1
 
-// Hand sensor sensitivity.
-// Higher = less sensitive.
-// Start at 40. If still too sensitive, try 60, 80, 100...
-#define HAND_THRESHOLD        40
+/*
+ * PHOTO sensitivity.
+ * Previous working value: 3.
+ * New value: 4 = approximately 30% lower sensitivity / shorter range.
+ */
+#define HAND_THRESHOLD       4
 
-// Hysteresis threshold for hand removal
-#define RELEASE_THRESHOLD     20
-
-#define HAND_CONFIRM_COUNT    3
+#define HAND_CONFIRM_COUNT   2
 #define RELEASE_CONFIRM_COUNT 2
 
-// Optical scan period
-#define SCAN_PERIOD_MS        40
+#define SCAN_PERIOD_MS       50
 
-// Dispense times
-#define MODE_CYAN_TIME_MS     1200UL
-#define MODE_WHITE_TIME_MS    2000UL
+/*
+ * Anti-repeat protection:
+ * after one dispense, another dispense is forbidden until
+ * 1) a minimum lockout time has elapsed, AND
+ * 2) the optical sensor has been continuously clear for REARM_CLEAR_MS.
+ * This prevents foam left under the nozzle from being interpreted as a new hand.
+ */
+#define POST_DISPENSE_LOCKOUT_MS 1000UL
+#define REARM_CLEAR_MS            700UL
+#define LOCKOUT_SCAN_COUNT        ((uint8_t)(POST_DISPENSE_LOCKOUT_MS / SCAN_PERIOD_MS))
+#define REARM_CLEAR_COUNT         ((uint8_t)(REARM_CLEAR_MS / SCAN_PERIOD_MS))
 
-// After mode change, sensor must see "no hand" repeatedly before arming
-#define ARM_RELEASE_COUNT     12
+// ONLY CHANGES IN THIS REVISION:
+//
+// Mode 1 = cyan = 0.5 s
+#define MODE1_TIME_MS        500UL
 
-// Approx. 3.3 V low battery threshold using internal 1.024 V FVR
-#define LOW_BATTERY_ADC       317
+// Mode 2 = white = 0.9 s
+#define MODE2_TIME_MS        900UL
+
+/*
+ * Low battery threshold.
+ * Approx. 3.3V for 3xAAA.
+ */
+#define LOW_BATTERY_ADC      317
+
+// ============================================================
+// MODES
+// ============================================================
 
 typedef enum
 {
     MODE_OFF = 0,
-    MODE_CYAN,
-    MODE_WHITE
+    MODE_2SEC,
+    MODE_3SEC
 } dispenser_mode_t;
 
 // ============================================================
@@ -142,7 +160,7 @@ static void motor_off(void)
 }
 
 // ============================================================
-// LEDS
+// RGB LED
 // ============================================================
 
 static void leds_off(void)
@@ -156,6 +174,12 @@ static void red_on(void)
 {
     leds_off();
     LED_RED = LED_ON_LEVEL;
+}
+
+static void blue_on(void)
+{
+    leds_off();
+    LED_BLUE = LED_ON_LEVEL;
 }
 
 static void cyan_on(void)
@@ -178,9 +202,10 @@ static void white_on(void)
 
 static uint16_t adc_convert(void)
 {
-    __delay_us(20);
+    __delay_us(25);
 
     ADCON0bits.GO_nDONE = 1;
+
     while (ADCON0bits.GO_nDONE)
     {
     }
@@ -195,95 +220,90 @@ static uint16_t adc_read_photo(void)
 }
 
 // ============================================================
-// HAND SENSOR
+// PHOTO / HAND SENSOR
 // ============================================================
 
 static uint16_t read_hand_signal(void)
 {
-    uint32_t off_sum = 0;
-    uint32_t on_sum  = 0;
-
-    uint16_t off_avg;
-    uint16_t on_avg;
-
+    uint16_t background;
+    uint16_t illuminated;
+    uint16_t diff;
+    uint16_t max_diff = 0;
     uint8_t i;
 
-    // Ambient light measurement
     ir_off();
-    __delay_us(150);
+    __delay_us(200);
 
-    for (i = 0; i < 4; i++)
-    {
-        off_sum += adc_read_photo();
-        __delay_us(60);
-    }
+    background = adc_read_photo();
 
-    off_avg = (uint16_t)(off_sum >> 2);
-
-    // IR illuminated measurement
     ir_on();
-    __delay_us(100);
 
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < 6; i++)
     {
-        on_sum += adc_read_photo();
-        __delay_us(60);
+        __delay_us(100);
+
+        illuminated = adc_read_photo();
+
+        if (illuminated >= background)
+        {
+            diff = illuminated - background;
+        }
+        else
+        {
+            diff = background - illuminated;
+        }
+
+        if (diff > max_diff)
+        {
+            max_diff = diff;
+        }
     }
 
     ir_off();
 
-    on_avg = (uint16_t)(on_sum >> 2);
-
-    if (on_avg >= off_avg)
-        return (on_avg - off_avg);
-    else
-        return (off_avg - on_avg);
+    return max_diff;
 }
+
+// ============================================================
+// HAND DETECTION WITH FILTERING
+// ============================================================
 
 static bool hand_present(void)
 {
+    static uint8_t detect_count = 0;
+    static uint8_t release_count = 0;
     static bool state = false;
-    static uint8_t on_count = 0;
-    static uint8_t off_count = 0;
 
-    uint16_t signal = read_hand_signal();
+    uint16_t signal;
 
-    if (!state)
+    signal = read_hand_signal();
+
+    if (signal >= HAND_THRESHOLD)
     {
-        if (signal >= HAND_THRESHOLD)
-        {
-            if (on_count < HAND_CONFIRM_COUNT)
-                on_count++;
+        release_count = 0;
 
-            if (on_count >= HAND_CONFIRM_COUNT)
-            {
-                state = true;
-                on_count = 0;
-                off_count = 0;
-            }
-        }
-        else
+        if (detect_count < HAND_CONFIRM_COUNT)
         {
-            on_count = 0;
+            detect_count++;
+        }
+
+        if (detect_count >= HAND_CONFIRM_COUNT)
+        {
+            state = true;
         }
     }
     else
     {
-        if (signal <= RELEASE_THRESHOLD)
-        {
-            if (off_count < RELEASE_CONFIRM_COUNT)
-                off_count++;
+        detect_count = 0;
 
-            if (off_count >= RELEASE_CONFIRM_COUNT)
-            {
-                state = false;
-                on_count = 0;
-                off_count = 0;
-            }
-        }
-        else
+        if (release_count < RELEASE_CONFIRM_COUNT)
         {
-            off_count = 0;
+            release_count++;
+        }
+
+        if (release_count >= RELEASE_CONFIRM_COUNT)
+        {
+            state = false;
         }
     }
 
@@ -291,24 +311,25 @@ static bool hand_present(void)
 }
 
 // ============================================================
-// BATTERY
+// BATTERY MEASUREMENT USING INTERNAL FVR
 // ============================================================
 
 static uint16_t read_battery_fvr_adc(void)
 {
     uint16_t value;
 
-    // FVREN = 1, ADFVR = 01 => 1.024 V
     FVRCON = 0x81;
+
     __delay_ms(2);
 
-    // Internal FVR ADC channel
     ADCON0bits.CHS = 0b11111;
-    __delay_us(40);
+
+    __delay_us(50);
 
     value = adc_convert();
 
     ADCON0bits.CHS = 0b00000;
+
     FVRCON = 0x00;
 
     return value;
@@ -316,8 +337,21 @@ static uint16_t read_battery_fvr_adc(void)
 
 static bool battery_low(void)
 {
-    return (read_battery_fvr_adc() >= LOW_BATTERY_ADC);
+    uint16_t value;
+
+    value = read_battery_fvr_adc();
+
+    if (value >= LOW_BATTERY_ADC)
+    {
+        return true;
+    }
+
+    return false;
 }
+
+// ============================================================
+// LOW BATTERY INDICATION
+// ============================================================
 
 static void low_battery_flash(void)
 {
@@ -327,6 +361,7 @@ static void low_battery_flash(void)
     {
         red_on();
         __delay_ms(180);
+
         leds_off();
         __delay_ms(180);
     }
@@ -338,23 +373,32 @@ static void low_battery_flash(void)
 
 static void show_mode(dispenser_mode_t mode)
 {
-    if (mode == MODE_CYAN)
+    switch (mode)
     {
-        cyan_on();
-        __delay_ms(700);
-    }
-    else if (mode == MODE_WHITE)
-    {
-        white_on();
-        __delay_ms(700);
-    }
-    else
-    {
-        red_on();
-        __delay_ms(700);
-    }
+        case MODE_2SEC:
 
-    leds_off();
+            cyan_on();
+            __delay_ms(700);
+            leds_off();
+
+            break;
+
+        case MODE_3SEC:
+
+            white_on();
+            __delay_ms(700);
+            leds_off();
+
+            break;
+
+        default:
+
+            red_on();
+            __delay_ms(700);
+            leds_off();
+
+            break;
+    }
 }
 
 // ============================================================
@@ -371,37 +415,13 @@ static bool mode_touch_event(void)
         {
             while (MODE_SENSOR == MODE_ACTIVE_LEVEL)
             {
-                motor_off();
                 __delay_ms(10);
             }
 
             __delay_ms(30);
+
             return true;
         }
-    }
-
-    return false;
-}
-
-// ============================================================
-// ARMING
-// ============================================================
-
-static bool wait_sensor_clear_nonblocking(uint8_t *counter)
-{
-    bool hand = hand_present();
-
-    if (!hand)
-    {
-        if (*counter < ARM_RELEASE_COUNT)
-            (*counter)++;
-
-        if (*counter >= ARM_RELEASE_COUNT)
-            return true;
-    }
-    else
-    {
-        *counter = 0;
     }
 
     return false;
@@ -411,85 +431,83 @@ static bool wait_sensor_clear_nonblocking(uint8_t *counter)
 // DISPENSE
 // ============================================================
 
-static void dispense(uint16_t maximum_time_ms)
+static void dispense(uint32_t maximum_time_ms)
 {
-    uint16_t elapsed = 0;
-    uint8_t release_count = 0;
+    uint32_t elapsed = 0;
+    uint8_t hand_release_count = 0;
 
     motor_on();
 
     while (elapsed < maximum_time_ms)
     {
-        // Stop immediately if mode sensor is touched
-        if (MODE_SENSOR == MODE_ACTIVE_LEVEL)
-            break;
-
-        // Stop quickly when hand is removed
         if (!hand_present())
         {
-            release_count++;
+            if (hand_release_count < 3)
+            {
+                hand_release_count++;
+            }
 
-            if (release_count >= 2)
+            if (hand_release_count >= 3)
+            {
                 break;
+            }
         }
         else
         {
-            release_count = 0;
+            hand_release_count = 0;
         }
 
         __delay_ms(SCAN_PERIOD_MS);
+
         elapsed += SCAN_PERIOD_MS;
     }
 
     motor_off();
+
     ir_off();
 
-    __delay_ms(80);
+    __delay_ms(100);
 
     if (battery_low())
+    {
         low_battery_flash();
+    }
 }
 
 // ============================================================
-// INIT
+// INITIALIZATION
 // ============================================================
 
 static void init_pic(void)
 {
-    // Internal oscillator 4 MHz
     OSCCONbits.IRCF = 0b1101;
     OSCCONbits.SCS  = 0b10;
 
-    // Safe output states first
-    LATCbits.LATC5 = MOTOR_OFF_LEVEL;
+    MOTOR = MOTOR_OFF_LEVEL;
+
+    LED_RED   = LED_OFF_LEVEL;
+    LED_GREEN = LED_OFF_LEVEL;
+    LED_BLUE  = LED_OFF_LEVEL;
 
 #if IR_ACTIVE_HIGH
-    LATCbits.LATC3 = 0;
+    IR_LED = 0;
 #else
-    LATCbits.LATC3 = 1;
+    IR_LED = 1;
 #endif
 
-    LATCbits.LATC2 = LED_OFF_LEVEL;
-    LATCbits.LATC0 = LED_OFF_LEVEL;
-    LATAbits.LATA2 = LED_OFF_LEVEL;
-
-    // Only RA0 analog
     ANSELA = 0b00000001;
     ANSELC = 0b00000000;
 
-    // RA0 input, RA2 output
     TRISA = 0b00000001;
 
-    // RC4 input; RC5/RC3/RC2/RC0 outputs
     TRISC = 0b00010000;
 
-    // ADC setup
-    ADCON1bits.ADFM   = 1;
+    ADCON1bits.ADFM = 1;
     ADCON1bits.ADPREF = 0b00;
     ADCON1bits.ADNREF = 0;
-    ADCON1bits.ADCS   = 0b010;
+    ADCON1bits.ADCS = 0b010;
 
-    ADCON0bits.CHS  = 0b00000;
+    ADCON0bits.CHS = 0b00000;
     ADCON0bits.ADON = 1;
 
     motor_off();
@@ -507,81 +525,134 @@ void main(void)
 {
     dispenser_mode_t mode = MODE_OFF;
 
-    bool armed = false;
     bool hand;
+    bool armed = true;
 
+    uint8_t detect_counter = 0;
     uint8_t clear_counter = 0;
+    uint8_t lockout_counter = 0;
 
     init_pic();
 
     while (1)
     {
-        // Outside dispense(), motor must always be OFF
-        motor_off();
-
-        // Mode switching
         if (mode_touch_event())
         {
             motor_off();
-            ir_off();
+
+            /* Changing mode also resets the hand-detection state. */
+            armed = true;
+            detect_counter = 0;
+            clear_counter = 0;
+            lockout_counter = 0;
 
             if (mode == MODE_OFF)
-                mode = MODE_CYAN;
-            else if (mode == MODE_CYAN)
-                mode = MODE_WHITE;
+            {
+                mode = MODE_2SEC;
+            }
+            else if (mode == MODE_2SEC)
+            {
+                mode = MODE_3SEC;
+            }
             else
+            {
                 mode = MODE_OFF;
+            }
 
             show_mode(mode);
 
-            // Require clear optical sensor before allowing dispense
-            armed = false;
-            clear_counter = 0;
+            __delay_ms(200);
 
             continue;
         }
 
-        // OFF mode: motor and IR are always off
         if (mode == MODE_OFF)
         {
             motor_off();
             ir_off();
 
-            armed = false;
+            armed = true;
+            detect_counter = 0;
             clear_counter = 0;
+            lockout_counter = 0;
 
             __delay_ms(20);
+
             continue;
         }
 
-        // Arming: wait until sensor clearly sees "no hand"
+        hand = hand_present();
+
+        /*
+         * DISARMED STATE (after dispensing):
+         * Never start the motor here, even if foam reflects the IR beam.
+         * First wait through the mandatory lockout, then require the sensor
+         * to be continuously clear before arming the dispenser again.
+         */
         if (!armed)
         {
-            motor_off();
+            detect_counter = 0;
 
-            if (wait_sensor_clear_nonblocking(&clear_counter))
+            if (lockout_counter > 0)
             {
-                armed = true;
+                lockout_counter--;
                 clear_counter = 0;
+            }
+            else
+            {
+                if (!hand)
+                {
+                    if (clear_counter < REARM_CLEAR_COUNT)
+                    {
+                        clear_counter++;
+                    }
+
+                    if (clear_counter >= REARM_CLEAR_COUNT)
+                    {
+                        armed = true;
+                        clear_counter = 0;
+                    }
+                }
+                else
+                {
+                    /* Foam/hand is still visible: stay locked indefinitely. */
+                    clear_counter = 0;
+                }
             }
 
             __delay_ms(SCAN_PERIOD_MS);
             continue;
         }
 
-        // Ready: hand appears -> start dispensing immediately
-        hand = hand_present();
-
+        /* ARMED STATE: normal hand detection. */
         if (hand)
         {
-            // Prevent repeat until hand is removed
-            armed = false;
-            clear_counter = 0;
+            if (detect_counter < 3)
+            {
+                detect_counter++;
+            }
 
-            if (mode == MODE_CYAN)
-                dispense(MODE_CYAN_TIME_MS);
-            else if (mode == MODE_WHITE)
-                dispense(MODE_WHITE_TIME_MS);
+            if (detect_counter >= 3)
+            {
+                /* Disarm BEFORE running the motor so no second dose can occur. */
+                armed = false;
+                detect_counter = 0;
+                clear_counter = 0;
+                lockout_counter = LOCKOUT_SCAN_COUNT;
+
+                if (mode == MODE_2SEC)
+                {
+                    dispense(MODE1_TIME_MS);
+                }
+                else
+                {
+                    dispense(MODE2_TIME_MS);
+                }
+            }
+        }
+        else
+        {
+            detect_counter = 0;
         }
 
         __delay_ms(SCAN_PERIOD_MS);
